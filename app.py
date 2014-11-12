edbb10f7a806beb2515e135f34bf6a59cc8e3b94
erickolbusz <erickolbusz@gmail.com>
2014-11-11 22:52:23 -0500

    all functionality done, just a matter of possible styling on the homepage which I tried to do and failed

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 import fetch
 
 app=Flask(__name__)
+app.secret_key = 'eMG>h4_S:<o<Ow;_+Ja&imp(sLrr)aQO{(#r_NQa~.|HkTlDEtW{,bVs[3B!i=sy'
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template("index.html", page_title="Home")
+    submit = request.args.get("submit")
+    if (submit == "View"):
+        session['crit1'] = request.args.get("criteria1")
+        session['crit2'] = request.args.get("criteria2")
+        session['crit3'] = request.args.get("criteria3")
+        session['crit4'] = request.args.get("criteria4")
+        session['crit5'] = request.args.get("criteria5")
+        session['state'] = request.args.get("state")
+        return redirect("/results")
+    
+    categories = []
+    for category in fetch.CATEGORY_FULLNAMES:
+        categories.append(fetch.CATEGORY_FULLNAMES[category])
+    categories.remove("State Name")
+
+    states = []
+    for state in fetch.STATES:
+        if len(fetch.STATES[state])>1:
+            states.append(fetch.STATES[state])
+    
+    return render_template("index.html", page_title="Home", categories=categories, states=states)
 
 # TODO restrict method to POST
 @app.route("/results", methods=['GET', 'POST'])
 def results():
     # TODO remove testing demo
-    state = "New York"
-    categories = [fetch.TOTAL_POPULATION, fetch.HOUSEHOLDS]
-    return render_template("results.html", data_dict=fetch.getSummary(state, categories), page_title="Results")
+    categories = []
+    for category in fetch.CATEGORY_FULLNAMES:
+        for i in range(5):
+            if (fetch.CATEGORY_FULLNAMES[category] == session['crit'+str(i+1)]):
+                categories.append(category)
+
+    return render_template("results.html", data_dict=fetch.getSummary(session.get('state'), categories), page_title="Results")
 
 if __name__ == "__main__":
     app.debug=True
